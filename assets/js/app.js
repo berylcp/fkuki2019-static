cc45a2dd4e8fe0b19def1dfb5b30cb2a396ad3aa
berylcp <berylcp@gmail.com>
2022-07-18 03:28:09 +0700

    Perbaikan

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-const navbarToggle = document.querySelector(".navbar__toggle input");
-const navbarNav = document.querySelector(".navbar .navbar__nav");
-
-navbarToggle.addEventListener("click", function () {
-  navbarNav.classList.toggle("slide");
-});
